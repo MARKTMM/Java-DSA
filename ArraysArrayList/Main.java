f57c2bc05c3171991122ba63181c1c85ab8c9198
MARKTMM <markmwangi304@gmail.com>
2023-09-27 13:59:58 +0300

    Finally doing arrays

---
+++ ArraysArrayList/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        // Remember this file is just for fun.
+        // It's main purpose is to show us the do's and dont's
+
+        // Q1: Store a roll number
+        int a= 19;
+
+        // Q2: Store a person's name
+        String name = "Mark Mwangi";
+
+        // Q3: Store 5 roll numbers
+        int rno1 = 23;
+        int rno2 = 23;
+        int rno3 = 23;
+        int rno4 = 23;
+        int rno5 = 23;
+    }
+}
