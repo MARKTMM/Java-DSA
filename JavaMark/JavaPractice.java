69f31a9b6abb2b92d7c3de90f4c192b801bb5f1c
Mark Mwangi <mark@Mark.www.itsmark.com>
2024-01-30 23:50:51 +0300

    Java

--- JavaMark/JavaPractice.java
+++ JavaMark/JavaPractice.java
@@ -1,7 +1,26 @@
 package JavaMark;
 
+import java.util.Arrays;
+import java.util.Date;
+
 public class JavaPractice{
 	public static void main(String[] args) {
-		System.out.println("Mark");
+		System.out.println("Testing to see whether it works");
+		Date now = new Date();
+		System.out.println(now);
+
+		// Arrays
+		int[] numbers = new int[6];
+		numbers[0] = 4;
+		numbers[1] = 4;
+		numbers[2] = 4;
+		numbers[3] = 4;
+		numbers[4] = 4;
+		numbers[5] = 4;
+		System.out.println(Arrays.toString(numbers));
+
+		int temprature = 12;
+		boolean isWarm = temprature > 20 && temprature < 30;
+		System.out.println(isWarm);
 	}
 }
---
+++ out/production/Java-DSA/ArraysArrayList/README.md
@@ -0,0 +1,4 @@
+In this directory i'll be tackling arrays.
+I'll try commenting as much as possible to guide you through my journey.
+Arrays are quite intresting!
+NB/ The Main.java file is just for fun.
